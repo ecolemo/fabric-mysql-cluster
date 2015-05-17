07fb7a099cd59c696e498824c40f50403c2a616c
Enoch Lee <enoch2110me.com>
2015-05-17 21:11:54 +0900

    update .gitignore fixed setup_sql_node procedure improved env.roledefs to enable separate definitions of ssh hosts and node hosts for the nodes

--- fabfile.py
+++ fabfile.py
@@ -6,40 +6,55 @@ from fabric.operations import put, run
 from fabric.state import env
 from fabric.tasks import execute
 
-env.user = 'root'
+
 env.roledefs = {
-    'mgm_nodes': ['10.211.55.33'],
-    'data_nodes': ['10.211.55.34'],
-    'sql_nodes': ['10.211.55.35'],
+    'mgm_nodes': {
+        'hosts': ['10.211.55.36'],
+        'node_hosts': ['192.168.0.1']
+    },
+    'data_nodes': {
+        'hosts': ['10.211.55.37', '10.211.55.38'],
+        'node_hosts': ['192.168.0.2', '192.168.0.3']
+    },
+    'sql_nodes': {
+        'hosts': ['10.211.55.39'],
+        'node_hosts': ['192.168.0.4']
+    }
 }
+
+env.user = 'root'
 
 def create_conf_files():
     configurations = []
+    node_hosts = {}
+    for node_type in ['mgm_nodes', 'data_nodes', 'sql_nodes']:
+        node_hosts[node_type] = env.roledefs[node_type]['node_hosts'] if env.roledefs[node_type]['node_hosts'] else env.roledefs[node_type]['hosts']
+
     # configuation for config.ini
     configuration_1 = {}
     configuration_1['file'] = 'config.ini'
     configuration_1['replacements'] = {
-        '<num_of_replicas>': str(len(env.roledefs['data_nodes'])),
+        '<num_of_replicas>': str(len(node_hosts['data_nodes'])),
         '<mgm_node>': '',
         '<data_node>': '',
         '<sql_node>': ''
     }
 
-    for host in env.roledefs['mgm_nodes']:
-        configuration_1['replacements']['<mgm_node>'] = "[ndb_mgmd]\nhostname=%s\ndatadir=/var/lib/mysql-cluster\n" % host
+    for host in node_hosts['mgm_nodes']:
+        configuration_1['replacements']['<mgm_node>'] += "[ndb_mgmd]\nhostname=%s\ndatadir=/var/lib/mysql-cluster\n\n" % host
 
-    for host in env.roledefs['data_nodes']:
-        configuration_1['replacements']['<data_node>'] = "[ndbd]\nhostname=%s\ndatadir=/usr/local/mysql/data\n" % host
+    for host in node_hosts['data_nodes']:
+        configuration_1['replacements']['<data_node>'] += "[ndbd]\nhostname=%s\ndatadir=/usr/local/mysql/data\n\n" % host
 
-    for host in env.roledefs['sql_nodes']:
-        configuration_1['replacements']['<sql_node>'] = "[mysqld]\nhostname=%s\n" % host
+    for host in node_hosts['sql_nodes']:
+        configuration_1['replacements']['<sql_node>'] += "[mysqld]\nhostname=%s\n\n" % host
 
     configurations.append(configuration_1)
 
     # configuration for my.cnf
     configuration_2 = {}
     configuration_2['file'] = 'my.cnf'
-    configuration_2['replacements'] = {'<mgm_node_ip>': env.roledefs['mgm_nodes'][0]}
+    configuration_2['replacements'] = {'<mgm_node_ip>': node_hosts['mgm_nodes'][0]}
     configurations.append(configuration_2)
 
     for configuration in configurations:
@@ -53,7 +68,6 @@ def create_conf_files():
 
         infile.close()
         outfile.close()
-
 
 @roles("mgm_nodes")
 def setup_mgm_nodes():
@@ -77,10 +91,9 @@ def setup_sql_nodes():
     run('chmod +x /var/tmp/sqlnode.sh')
     run('/var/tmp/sqlnode.sh')
 
-
 @roles("mgm_nodes")
 def start_mgm_nodes():
-    run('/usr/local/bin/ndb_mgmd -f /var/lib/mysql-cluster/config.ini --configdir=/var/lib/mysql-cluster')
+    run('/usr/local/bin/ndb_mgmd -f /var/lib/mysql-cluster/config.ini --configdir=/var/lib/mysql-cluster --initial')
 
 @roles("data_nodes")
 def start_data_nodes():
@@ -95,7 +108,6 @@ def setup_mysql_cluster():
     execute(setup_mgm_nodes)
     execute(setup_data_nodes)
     execute(setup_sql_nodes)
-
 
 def start_mysql_cluster():
     execute(start_mgm_nodes)
